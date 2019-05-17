650d38be22f2041b589e6c5b58a7c9c97afd2e4f
SergeiMinaev <brainsburn@yandex.ru>
2019-05-18 02:40:21 +0300

    Add BASE_DIR

--- norobot.py
+++ norobot.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
-
+import os
 import random
 from PIL import Image, ImageFont, ImageDraw, ImageEnhance, ImageOps
 
+
+BASE_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)))
 
 img_width = 280
 img_height = 60
 noise_color_range=(130,200)
 word_color_range=(30,110)
 letter_offset=30
-words_file = "words_ru/words.txt"
+words_file = os.path.join(BASE_DIR, "words_ru/words.txt")
 fonts = ["fonts/betina-script-ctt.ttf", "fonts/sign-painter.otf"]
 noise = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
 noise_count = 60
+
 
 def gen_captcha():
     with open(words_file) as f:
@@ -29,7 +32,9 @@ def gen_captcha():
         d = ImageDraw.Draw(letter_img)
         d.text((0,0),
                 letter,
-                font=ImageFont.truetype(random.choice(fonts), random.randrange(25,35)),
+                font=ImageFont.truetype(
+                    os.path.join(BASE_DIR, random.choice(fonts)),
+                    random.randrange(25,35)),
                 fill=255)
         w = letter_img.rotate(random.randrange(1,190), expand=1)
         img.paste( ImageOps.colorize(w, (0,0,0),
@@ -56,7 +61,9 @@ def gen_captcha():
         d = ImageDraw.Draw(letter_img)
         d.text((3,3),
                 letter,
-                font=ImageFont.truetype(random.choice(fonts), 40),
+                font=ImageFont.truetype(
+                    os.path.join(BASE_DIR,random.choice(fonts)),
+                    40),
                 fill=255)
         w = letter_img.rotate(random.randrange(-30,30), expand=1)
         img.paste( ImageOps.colorize(w, color,color),
