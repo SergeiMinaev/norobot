ded63151e6106ea6a58a2604ee34871a9f06ac94
SergeiMinaev <brainsburn@yandex.ru>
2019-05-18 17:02:09 +0300

    Small improvements; handle 'width' argument

--- norobot.py
+++ norobot.py
@@ -6,18 +6,20 @@ from PIL import Image, ImageFont, ImageDraw, ImageEnhance, ImageOps
 
 BASE_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)))
 
-img_width = 280
+img_width = 300
 img_height = 60
-noise_color_range=(130,200)
-word_color_range=(30,110)
-letter_offset=30
+noise_color_range = (140,200)
+word_color_range = (30,100)
+word_font_size = 45
+letter_offset = 33
 words_file = os.path.join(BASE_DIR, "words_ru/words.txt")
 fonts = ["fonts/betina-script-ctt.ttf", "fonts/sign-painter.otf"]
 noise = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
 noise_count = 60
+noise_font_size_range = (35,40)
 
 
-def gen_captcha():
+def gen_captcha(width = img_width):
     with open(words_file) as f:
         lines = f.readlines()
         word = random.choice(lines)
@@ -25,6 +27,7 @@ def gen_captcha():
 
     img = Image.new('RGB', (img_width, img_height), (255, 255, 255))
 
+    #word = "велоджинс"
     n = 0
     while n < noise_count:
         letter = random.choice(noise)
@@ -34,7 +37,7 @@ def gen_captcha():
                 letter,
                 font=ImageFont.truetype(
                     os.path.join(BASE_DIR, random.choice(fonts)),
-                    random.randrange(25,35)),
+                    random.randrange(noise_font_size_range[0],noise_font_size_range[1])),
                 fill=255)
         w = letter_img.rotate(random.randrange(1,190), expand=1)
         img.paste( ImageOps.colorize(w, (0,0,0),
@@ -45,9 +48,9 @@ def gen_captcha():
         n += 1
 
     offset = img_width / 2
-    offset = int(offset-(letter_offset*(len(word)/2))+5)
+    offset = int(offset-(letter_offset*(len(word)/2))+2)
     try:
-        offset = random.randrange(5,offset)
+        offset = random.randrange(2,offset)
     except:
         pass
 
@@ -57,16 +60,17 @@ def gen_captcha():
 
     for letter in word:
         letter = letter.upper()
-        letter_img = Image.new('L', (50,50))
+        letter_img = Image.new('L', (int(word_font_size*1.2), int(word_font_size*1.2)))
         d = ImageDraw.Draw(letter_img)
         d.text((3,3),
                 letter,
                 font=ImageFont.truetype(
                     os.path.join(BASE_DIR,random.choice(fonts)),
-                    40),
+                    word_font_size),
                 fill=255)
         w = letter_img.rotate(random.randrange(-30,30), expand=1)
         img.paste( ImageOps.colorize(w, color,color),
-                (offset, random.randrange(1,10)),  w)
+                (offset, random.randrange(-1,3)),  w)
         offset += letter_offset
+    img = img.resize((width, int(width * img_height / img_width)), Image.ANTIALIAS)
     return {'img': img, 'secret': word}
